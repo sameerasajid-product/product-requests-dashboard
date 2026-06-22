6a272cb8b43695e8bfca4fc73a043bb1f0193807
sameerasajid-product <sameera.sajid@numbers.pk>
2026-06-22 09:57:59 +0500

    FIX LOGO

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,6 +7,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|api/.*).*)",
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:png|jpg|jpeg|gif|svg|webp|ico)$|api/.*).*)",
   ],
 };
